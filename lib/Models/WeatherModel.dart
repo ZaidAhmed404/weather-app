5d4fa3fb5c29677989bdb2d491912f69ca20bf02
Zaid Ahmed <zaidahmed@meraki-it.pk>
2024-04-21 23:39:00 +0500

    UI updated

--- lib/Models/WeatherModel.dart
+++ lib/Models/WeatherModel.dart
@@ -2,16 +2,31 @@ class WeatherModel {
   final String cityName;
   final double temperature;
   final String mainCondition;
+  final int code;
+  final double tempMin;
+  final double tempMax;
+  final double sunRise;
+  final double sunSet;
 
   WeatherModel(
       {required this.cityName,
       required this.mainCondition,
-      required this.temperature});
+      required this.temperature,
+      required this.code,
+      required this.tempMax,
+      required this.tempMin,
+      required this.sunRise,
+      required this.sunSet});
 
   factory WeatherModel.fromJson(Map<String, dynamic> json) {
     return WeatherModel(
-        cityName: json['name'],
+        cityName: json['name'] ?? "",
         temperature: json['main']['temp'].toDouble(),
-        mainCondition: json['weather'][0]['main']);
+        mainCondition: json['weather'][0]['main'] ?? "",
+        tempMax: json['main']['temp_min'].toDouble(),
+        tempMin: json['main']['temp_max'].toDouble(),
+        sunRise: json['sys']['sunrise'].toDouble(),
+        sunSet: json['sys']['sunset'].toDouble(),
+        code: json['cod']);
   }
 }
